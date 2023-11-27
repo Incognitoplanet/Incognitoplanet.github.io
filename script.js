2daddb133ca111c4932ef613858115d19b925b8f
Incognitoplanet <151534192+Incognitoplanet@users.noreply.github.com>
2023-11-27 11:36:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,93 @@
+// Initial setup
 var password = "Parzival";
 var resetPassword = "first-to-the-key";
 var isPasswordEntered = false;
-var scoreboard = [];
-var currentPoints = 100000;
+var scoreboard = JSON.parse(localStorage.getItem('scoreboard')) || [];
+var currentPoints = parseInt(localStorage.getItem('currentPoints')) || 100000;
 
+// Function to handle document load
 document.addEventListener("DOMContentLoaded", function () {
-  if (localStorage.getItem('isPasswordEntered') === 'true') {
-    isPasswordEntered = true;
-  }
-
-  if (!isPasswordEntered) {
-    openPasswordModal(); // Focus on the password input when the page loads
+  if (!localStorage.getItem('isPasswordEntered')) {
+    openModal('passwordModal', 'password');
   }
 });
 
-function openPasswordModal() {
-  document.getElementById('passwordModal').style.display = 'flex';
-  document.getElementById('password').value = '';
+// Function to open a modal
+function openModal(modalId, focusInputId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'flex';
 
-  var passwordInput = document.getElementById('password');
-  passwordInput.focus(); // Focus on the password input
-
-  passwordInput.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-      processPassword();
-    }
-  });
-}
-
-function openUsernameModal() {
-  document.getElementById('usernameModal').style.display = 'flex';
-  document.getElementById('username').value = '';
-
-  var usernameInput = document.getElementById('username');
-  usernameInput.focus(); // Focus on the username input
-
-  usernameInput.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-      processUsername();
-    }
-  });
-}
-
-function openInvalidPasswordModal() {
-  document.getElementById('invalidPassword').style.display = 'flex';
-}
-
-function openErrorMessageModal() {
-  document.getElementById('errorMessageModal').style.display = 'flex';
-}
-
-function openUsernameExistsModal() {
-  document.getElementById('usernameExistsModal').style.display = 'flex';
-}
-
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = 'none';
-  if (modalId === 'usernameExistsModal' || modalId === 'invalidPassword' || modalId === 'errorMessageModal') {
-    openPasswordModal(); // Focus on the password input after closing these modals
+  if (focusInputId) {
+    var input = document.getElementById(focusInputId);
+    input.value = '';
+    input.focus();
   }
 }
 
+// Function to close a modal
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'none';
+}
+
+// Function to open password modal
+function openPasswordModal() {
+  openModal('passwordModal', 'password');
+}
+
+// Function to close password modal
+function closePasswordModal() {
+  closeModal('passwordModal');
+}
+
+// Function to open username modal
+function openUsernameModal() {
+  openModal('usernameModal', 'username');
+}
+
+// Function to open invalid password modal
+function openInvalidPasswordModal() {
+  openModal('invalidPassword');
+}
+
+// Function to open error message modal
+function openErrorMessageModal() {
+  openModal('errorMessageModal');
+}
+
+// Function to open username exists modal
+function openUsernameExistsModal() {
+  openModal('usernameExistsModal');
+}
+
+// Function to process password
 function processPassword() {
   var enteredPassword = document.getElementById('password').value;
 
   if (enteredPassword === password) {
     isPasswordEntered = true;
     localStorage.setItem('isPasswordEntered', 'true');
-    closeModal('passwordModal');
+    closePasswordModal();
     openUsernameModal();
-    return;  // Exit the function to prevent further execution
   } else if (enteredPassword === resetPassword) {
     resetScoreboard();
-    closeModal('passwordModal');
-    return;  // Exit the function to prevent further execution
+    closePasswordModal();
   } else {
     openInvalidPasswordModal();
-    return;  // Exit the function to prevent further execution
   }
 }
 
+// Function to process username
 function processUsername() {
   var enteredUsername = document.getElementById('username').value;
 
-  var usernameExists = scoreboard.some(function(entry) {
-    // Check if the entered username already exists in a case-insensitive manner
+  var usernameExists = scoreboard.some(function (entry) {
     return entry.username.toLowerCase() === enteredUsername.toLowerCase();
   });
 
-  if (!usernameExists && currentPoints > 0 && enteredUsername) {
+  if (usernameExists && scoreboard.length > 0) {
+    openUsernameExistsModal();
+  } else if (!usernameExists && currentPoints > 0 && enteredUsername) {
     addToScoreboard(enteredUsername, currentPoints);
     currentPoints = Math.floor(currentPoints / 2);
 
@@ -97,23 +96,26 @@ function processUsername() {
 
     updateScoreboardDisplay();
     closeModal('usernameModal');
-  } else if (usernameExists) {
-    openUsernameExistsModal();
   } else if (currentPoints <= 0) {
     openErrorMessageModal();
   }
+
+  // Close all modals after processing username
+  closeModal('usernameExistsModal');
+  closeModal('invalidPassword');
+  closeModal('errorMessageModal');
 }
 
+// Function to add entry to scoreboard
 function addToScoreboard(username, points) {
-  scoreboard.push({
-    username: username,
-    points: points
-  });
+  scoreboard.push({ username: username, points: points });
 }
 
+// Function to update scoreboard display
 function updateScoreboardDisplay() {
   var scoreboardList = document.getElementById("scoreboard");
   scoreboardList.innerHTML = "";
+
   scoreboard.forEach(function (entry) {
     var listItem = document.createElement("li");
     listItem.classList.add("red-text");
@@ -133,11 +135,13 @@ function updateScoreboardDisplay() {
   });
 }
 
+// Function to toggle password visibility
 function togglePasswordVisibility(inputId) {
   var passwordInput = document.getElementById(inputId);
   passwordInput.type = (passwordInput.type === 'password') ? 'text' : 'password';
 }
 
+// Function to reset the scoreboard
 function resetScoreboard() {
   scoreboard = [];
   currentPoints = 100000;
@@ -146,21 +150,46 @@ function resetScoreboard() {
   updateScoreboardDisplay();
 }
 
-if (localStorage.getItem('scoreboard')) {
-  scoreboard = JSON.parse(localStorage.getItem('scoreboard'));
-}
+// Event listener for scrolling to scoreboard
+document.querySelector('.square-button').addEventListener('click', function () {
+  document.getElementById('scoreboardContainer').scrollIntoView({ behavior: 'smooth' });
+});
 
-if (localStorage.getItem('currentPoints')) {
-  currentPoints = parseInt(localStorage.getItem('currentPoints'));
-}
+// Event listener for Copper Key button hover effect
+var copperKeyButton = document.getElementById('copperKeyButton');
+copperKeyButton.addEventListener('mouseenter', function () {
+  copperKeyButton.style.transform = 'scale(1.1)';
+});
 
-document.addEventListener('keyup', function (event) {
+copperKeyButton.addEventListener('mouseleave', function () {
+  copperKeyButton.style.transform = 'scale(1)';
+});
+
+// Event listener for Copper Key button click
+copperKeyButton.addEventListener('click', function () {
+  openPasswordModal();
+});
+
+// Event listeners for button clicks
+document.getElementById('copperKeyButton').addEventListener('click', function () {
+  openPasswordModal();
+});
+
+document.getElementById('submitButton').addEventListener('click', function () {
+  processPassword();
+});
+
+document.getElementById('usernameSubmitButton').addEventListener('click', function () {
+  processUsername();
+});
+
+// Additional event listener for Enter key in modals
+document.addEventListener('keydown', function (event) {
   if (event.key === 'Enter') {
     if (document.getElementById('passwordModal').style.display === 'flex') {
       processPassword();
     } else if (document.getElementById('usernameModal').style.display === 'flex') {
       processUsername();
     }
-    // Add other modal handling here if needed
   }
 });
