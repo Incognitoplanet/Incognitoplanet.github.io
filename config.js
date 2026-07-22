e44f6f85fa109b59b37505ed9cd6551fb5b44397
Incognitoplanet <151534192+Incognitoplanet@users.noreply.github.com>
2026-07-22 07:12:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-/* Leave blank for a normal https://USERNAME.github.io/REPOSITORY/ site.
-   Fill owner/repo when using a custom domain. */
 window.GALLERY_CONFIG = {
-  owner: "https://incognitoplanet.com/",
-  repo: "",
-  branch: "",
+  owner: "Incognitoplanet",
+  repo: "Incognitoplanet.github.io",
+  branch: "main",
   folder: "incog"
 };
